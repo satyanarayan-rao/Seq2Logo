5e7eed2c619484ef3ebcb8dddd9c0f823f6efb98
satyausc <satyanar@usc.edu>
2016-11-23 16:07:45 -0800

    Inital commit.

---
+++ R/generateSeqLogoForAsequence.R
@@ -0,0 +1,35 @@
+generateSeqLogoForAsequence <- function (sequence = "AAACCTAA", encodingFile = NULL, image_file_name = "pwm.png"){
+    seq = sequence
+    if (is.null(encodingFile)) {
+        encodingFile = system.file("extdata", "oneHotEncode.txt", package = "Seq2Logo")
+    }
+    encodedMatrix = OneHotEncode(seq, encodingFile)
+    pwm = makePWM(encodedMatrix)
+    png (filename = image_file_name,
+         res = 96)
+    seqLogo(pwm)
+    dev.off()
+}
+OneHotEncode <- function (sequence, encodingFile){
+    encodingdf = read.csv(encodingFile,
+                          sep = "",
+                          comment.char = "#",
+                          colClasses = "character")
+
+    rownames(encodingdf) = encodingdf [["Letter"]]
+    encodedString = matrix(nrow = dim(encodingdf)[1])
+    encodedString = encodedString[apply (encodedString,
+                                         2,
+                                         function(x) all(is.finite(x)))]
+    sequence = toupper(sequence)
+    splittedseq = base::unlist( base::strsplit(sequence, split = ""))
+    #print (splittedseq)
+    for (letter in splittedseq){
+        column = as.matrix(as.numeric (unlist(strsplit(encodingdf[letter, "Encoding"], split = ""))))
+        encodedString = cbind(encodedString, column)
+    }
+    encodedString = as.data.frame(encodedString )
+    #print (encodedString)
+    return (encodedString)
+
+}
